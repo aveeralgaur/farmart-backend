ab8109190596e84340f241ae3b8c145e2632e700
Aveeral Gaur <agaur@macs-MacBook-Pro.local>
2023-08-18 15:02:49 +0530

    Typeorm changes

--- dist/typeOrm.config.js
+++ dist/typeOrm.config.js
@@ -4,13 +4,13 @@ exports.getDatabaseConfig = void 0;
 const files_entity_1 = require("./upload-file/entities/files.entity");
 const getDatabaseConfig = async (configService) => {
     return {
-        type: 'mysql',
-        host: configService.get('DB_HOSTNAME') || 'localhost',
-        port: configService.get('DB_PORT') || 3306,
-        username: configService.get('DB_USERNAME') || 'root',
-        password: configService.get('DB_PASSWORD') || 'password',
-        database: configService.get('DB_NAME') || 'farmart',
-        entities: ['dist/**/*.entity{.ts,.js}', files_entity_1.files],
+        type: "mysql",
+        host: configService.get("DB_HOSTNAME") || "sql.freedb.tech",
+        port: configService.get("DB_PORT") || 3306,
+        username: configService.get("DB_USERNAME") || "freedb_file-upload-user",
+        password: configService.get("DB_PASSWORD") || "KQkS*fD#S!S2fpM",
+        database: configService.get("DB_NAME") || "freedb_fileupload",
+        entities: ["dist/**/*.entity{.ts,.js}", files_entity_1.files],
         synchronize: true,
     };
 };
--- src/typeOrm.config.ts
+++ src/typeOrm.config.ts
@@ -1,18 +1,19 @@
-import { TypeOrmModuleOptions } from '@nestjs/typeorm';
-import { ConfigService } from '@nestjs/config';
-import { files } from './upload-file/entities/files.entity';
+import { TypeOrmModuleOptions } from "@nestjs/typeorm";
+import { ConfigService } from "@nestjs/config";
+import { files } from "./upload-file/entities/files.entity";
 
 export const getDatabaseConfig = async (
-  configService: ConfigService,
+  configService: ConfigService
 ): Promise<TypeOrmModuleOptions> => {
   return {
-    type: 'mysql',
-    host: configService.get<string>('DB_HOSTNAME') || 'localhost',
-    port: configService.get<number>('DB_PORT') || 3306,
-    username: configService.get<string>('DB_USERNAME') || 'root',
-    password: configService.get<string>('DB_PASSWORD') || 'password',
-    database: configService.get<string>('DB_NAME') || 'farmart',
-    entities: ['dist/**/*.entity{.ts,.js}', files],
+    type: "mysql",
+    host: configService.get<string>("DB_HOSTNAME") || "sql.freedb.tech",
+    port: configService.get<number>("DB_PORT") || 3306,
+    username:
+      configService.get<string>("DB_USERNAME") || "freedb_file-upload-user",
+    password: configService.get<string>("DB_PASSWORD") || "KQkS*fD#S!S2fpM",
+    database: configService.get<string>("DB_NAME") || "freedb_fileupload",
+    entities: ["dist/**/*.entity{.ts,.js}", files],
     synchronize: true,
   };
 };
